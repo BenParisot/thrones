44f6fba5e080905c085bab0081483f4fa9fad367
Andrea Wilkowski and Ben Parisot <andrea@>
2017-10-25 20:04:08 -0700

    add all backend and ui scripts, hooray

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,65 @@
+//backend logic
+
 var house1 = 0;
 var house2 = 0;
 var house3 = 0;
 var house4 = 0;
 
+//userinterface logic
+
 $(document).ready(function() {
-  $("#animals").submit(function(event) {
+  $("#quiz").submit(function(event) {
 
-    var animal = $("#user-response").val();
+    var q1 = $("#weapon").val();
+    var q2 = $("#animal").val();
+    var q3 = $("#incest").val();
 
-    if (animal === "1") {
-      $('#elephant').hide();
-      $('#loris').hide();
-      $('#mantis').show();
-    } else if (animal === "2") {
 
-      $('#mantis').hide();
-      $('#loris').hide();
-      $('#elephant').show();
 
-    } else if (animal === "3"){
-      $('#elephant').hide();
-      $('mantis').hide();
-      $('#loris').show();
+    if (q1 === "1") {
+      house1 += 1;
+    } else if (q1 === "2") {
+      house2 += 1;
+    } else if (q1 === "3"){
+      house3 += 1;
+    } else if (q1 === "4") {
+      house4 += 1;
+    }
 
-    } else if (animal === "2") {
+    if (q2 === "1") {
+      house1 += 1;
+    } else if (q2 === "2") {
+      house2 += 1;
+    } else if (q2 === "3"){
+      house3 += 1;
+    } else if (q2 === "4") {
+      house4 += 1;
+    }
 
-      $('#mantis').hide();
-      $('#loris').hide();
-      $('#elephant').show();
+    if (q3 === "1") {
+      house1 += 1;
+    } else if (q3 === "2") {
+      house2 += 1;
+    } else if (q3 === "3"){
+      house3 += 1;
+    } else if (q3 === "4") {
+      house4 += 1;
+    }
 
+    //console.log("House2:" + house2);
+
+
+    if (house1 > house2 && house1 > house3 && house1 > house4) {
+      $("#stark").show();
+    } else if (house2 > house1 && house2 > house3 && house2 > house4) {
+      $("#lannister").show();
+    } else if (house3 > house1 && house3 > house2 && house3 > house4) {
+      $("#targaryen").show();
+    } else if (house4 > house1 && house4 > house3 && house4 > house2) {
+      $("#greyjoy").show();
+    } else {
+      $("#none").show();
+    }
 
     event.preventDefault();
   });
